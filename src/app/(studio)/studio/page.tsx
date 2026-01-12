e63c4a8c22272f32f4107c0f9b429d427f9047a2
Inam <mfihassan99@gmail.com>
2026-01-12 18:53:30 +0530

    fix: build fail

--- src/app/(studio)/studio/page.tsx
+++ src/app/(studio)/studio/page.tsx
@@ -2,6 +2,8 @@ import { PAGINATION_DEFAULT_LIMIT } from "@/constants";
 import { StudioView } from "@/modules/studio/ui/views/studio-view";
 import { getQueryClient, HydrateClient, trpc } from "@/trpc/server";
 
+export const dynamic = "force-dynamic";
+
 export default async function Page() {
   const queryClient = getQueryClient();
   void queryClient.prefetchInfiniteQuery(
